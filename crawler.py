49e19e1c6272f139a046e302dce90b920eec082a
luick klippel <gelberdev@gmail.com>
2019-03-05 14:08:56 +0100

    adding keywords.txt

--- crawler.py
+++ crawler.py
@@ -1,10 +1,21 @@
 import git
+import re
 from flashtext import KeywordProcessor
 
 keyword_processor = KeywordProcessor()
 
 repo = git.Repo("../wifi_activity_logger")
-keyword_processor.add_keyword('connect')
+
+keywords = []
+
+with open('keywords.txt') as file:
+    keywords = re.findall(r"([a-zA-Z\-]+)", file.read())
+
+print('Keywords: ')
+print(keywords)
+
+#keyword_processor.add_keyword('connect')
+keyword_processor.add_keywords_from_list(keywords)
 
 commits_list = list(repo.iter_commits())
 
@@ -19,7 +30,6 @@ for i in range(len(commits_list)):
 
 	diff = a_commit.diff(b_commit, create_patch=True)
 
-	# db.connect('secret_pw', 'google.com')
 	for diff in diff.iter_change_type('M'):
 		try:
 			commit_diff = diff.a_blob.data_stream.read().decode('utf-8')
@@ -30,6 +40,8 @@ for i in range(len(commits_list)):
 			print("Err")
 
 all_commits_lines = all_commits.splitlines()
+
+print('Results: ')
 for f in all_commits_lines:
 	if len(keyword_processor.extract_keywords(f)) > 0:
 		print(f)
